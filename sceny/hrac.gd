1f1479eca8ff6f9b9a58a10f9e5a7c3e403438af
Zeman Jan <jan.zeman@comap-control.com>
2020-04-15 17:21:58 +0200

    Vic postav
    JIRA: no issue
    
    Signed-off-by: Zeman Jan <jan.zeman@comap-control.com>

--- sceny/hrac.gd
+++ sceny/hrac.gd
@@ -10,21 +10,21 @@ func _process(delta):
 	var smerPohybu = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		smerPohybu.x += 1
-		$AnimatedSprite.animation = "doprava"
+		$Lucisnik.animation = "doprava"
 	if Input.is_action_pressed("ui_left"):
 		smerPohybu.x -= 1
-		$AnimatedSprite.animation = "doleva"
+		$Lucisnik.animation = "doleva"
 	if Input.is_action_pressed("ui_down"):
 		smerPohybu.y += 1
-		$AnimatedSprite.animation = "dolu"
+		$Lucisnik.animation = "dolu"
 	if Input.is_action_pressed("ui_up"):
 		smerPohybu.y -= 1
-		$AnimatedSprite.animation = "nahoru"
+		$Lucisnik.animation = "nahoru"
 	if smerPohybu.length() > 0:
 		smerPohybu = smerPohybu.normalized() * rychlost
-		$AnimatedSprite.play()
+		$Lucisnik.play()
 	else:
-		$AnimatedSprite.stop()
+		$Lucisnik.stop()
 
 	position += smerPohybu * delta
 	position.x = clamp(position.x, 0, velikostOkna.x)
