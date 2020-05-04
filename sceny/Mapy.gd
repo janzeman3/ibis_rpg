39c9412084f5c6c98d0e7b63d805d48b4d132467
JanZeman3 <JanZeman3@gmail.com>
2020-05-04 17:20:23 +0200

    Mapy: nechodime na zakazana policka
    Signed-off-by: JanZeman3 <JanZeman3@gmail.com>

--- sceny/Mapy.gd
+++ sceny/Mapy.gd
@@ -1,10 +1,12 @@
 extends Node
 
+var kam_nemohu = [-1, 0, 17]
+
 func jeVolno(cilPozice):
 	var cil = $Mapa1.world_to_map(cilPozice)
 	var cil_type = $Mapa1.get_cellv(cil)
-
-	if cil_type==-1:
+	
+	if kam_nemohu.has(cil_type):
 		return false
 	else:
 		return true
