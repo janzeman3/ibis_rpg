a1101e95ec3c7e559a5f10b52c84e6510199b277
JanZeman3 <JanZeman3@gmail.com>
2020-05-11 16:49:50 +0200

    Update mapy - 11.5.2020
    Signed-off-by: JanZeman3 <JanZeman3@gmail.com>

--- sceny/Mapy.gd
+++ sceny/Mapy.gd
@@ -1,11 +1,11 @@
 extends Node
 
-var kam_nemohu = [-1, 0, 17]
+var kam_nemohu = [-1, 0, 17, 86 , 87,16, 31, 53, 51, 26, 27]
 
 func jeVolno(cilPozice):
 	var cil = $Mapa1.world_to_map(cilPozice)
 	var cil_type = $Mapa1.get_cellv(cil)
-	
+
 	if kam_nemohu.has(cil_type):
 		return false
 	else:
